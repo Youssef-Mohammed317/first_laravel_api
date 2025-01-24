2249d3faab269e97dfe5d9bee249c45e014f5ca1
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-01-24 19:47:02 +0200

    send an email when register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,35 +6,41 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use App\Mail\WelcomeMail;
+// use Illuminate\Auth\Events\Registered;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
         // $validated = $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|email|unique:users',
-        //     'password' => 'required|string|min:3|confirmed',
-        // ]);
-        $validated = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:3|confirmed',
-        ]);
-        if($validated->fails()) {
-            return response()->json(['error' => $validated->errors()], 422);
-        }
-        try{
+            //     'name' => 'required|string|max:255',
+            //     'email' => 'required|email|unique:users',
+            //     'password' => 'required|string|min:3|confirmed',
+            // ]);
+            $validated = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:3|confirmed',
+            ]);
+            if($validated->fails()) {
+                return response()->json(['error' => $validated->errors()], 422);
+            }
+            try{
 
-            $user = User::create($request->all()); // laravel hash password automatically
-            // Hash::make($validated['password']); // for hashing password
+                $user = User::create($request->all()); // laravel hash password automatically
+                // Hash::make($validated['password']); // for hashing password
 
-            $token = $user->createToken('auth_token')->plainTextToken; // for generating token
-
-            return response()->json([
-                'access_token' => $token,
-                'user' => $user
-            ], 200); // 200 is the default status code
+                if($user){
+                    $token = $user->createToken('auth_token')->plainTextToken; // for generating token
+                    // event(new Registered($user));
+                    Mail::to($user->email)->send(new WelcomeMail($user));
+                    return response()->json([
+                        'access_token' => $token,
+                        'user' => $user
+                    ], 200); // 200 is the default status code
+                }
 
         } catch (\Exception $e) {
             return response()->json([
