006c9f57d9f8d2c902a9a6c15125f8b7a3a5a96c
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-01-24 16:10:37 +0200

    model and migration file for comments and likes, adjust migration files and build database

--- database/migrations/2025_01_24_140907_create_likes_table.php
+++ database/migrations/2025_01_24_140907_create_likes_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('likes');
     }
 };
