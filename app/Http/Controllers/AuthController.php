903541dccc19848e46927dc07da62f645cb266ed
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-01-24 15:32:39 +0200

    build and test login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -42,5 +43,33 @@ class AuthController extends Controller
             ], 500);
         }
     }
-   
+    public function login(Request $request){
+        $validated = Validator::make($request->all(), [
+            'email' => 'required|email|exists:users',
+            'password' => 'required|string',
+        ]);
+        if($validated->fails()) {
+            return response()->json(['error' => $validated->errors()], 422);
+        }
+        try{
+
+            if(!Auth::attempt($request->all())){
+                return response()->json(['error' => 'Invalid credentials']);
+            }
+            $user = User::where('email',$request->email)->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken; // for generating token
+
+            return response()->json([
+                'access_token' => $token,
+                'user' => $user
+            ], 200); // 200 is the default status code
+
+        } catch (\Exception $e) {
+            return response()->json([
+                // 'message' => 'Something went wrong'
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
 }
