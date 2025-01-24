3a081985694cae620f64cb17e881f7c15e71e8f5
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-01-24 15:07:02 +0200

    using validator and send errors response for register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,24 +5,42 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
-        $validated = $request->validate([
+        // $validated = $request->validate([
+        //     'name' => 'required|string|max:255',
+        //     'email' => 'required|email|unique:users',
+        //     'password' => 'required|string|min:3|confirmed',
+        // ]);
+        $validated = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users',
             'password' => 'required|string|min:3|confirmed',
         ]);
+        if($validated->fails()) {
+            return response()->json(['error' => $validated->errors()], 422);
+        }
+        try{
 
-        $user = User::create($validated); // laravel hash password automatically
-        // Hash::make($validated['password']); // for hashing password
+            $user = User::create($request->all()); // laravel hash password automatically
+            // Hash::make($validated['password']); // for hashing password
 
-        $token = $user->createToken('auth_token')->plainTextToken; // for generating token
+            $token = $user->createToken('auth_token')->plainTextToken; // for generating token
 
-        return response()->json([
-            'access_token' => $token,
-            'user' => $user
-        ], 200); // 200 is the default status code
+            return response()->json([
+                'access_token' => $token,
+                'user' => $user
+            ], 200); // 200 is the default status code
+
+        } catch (\Exception $e) {
+            return response()->json([
+                // 'message' => 'Something went wrong'
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
+   
 }
