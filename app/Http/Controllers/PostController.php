1ee029bdaa2aec63042fcc565430712441a07790
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-01-24 17:05:15 +0200

    adjust auth of edit function and build delete and get all and get post functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,7 +24,6 @@ class PostController extends Controller
             $post->content = $request->content;
             $post->user_id = Auth::user()->id;
 
-
             // $post = Post::create();
 
             if($post->save()){
@@ -52,6 +51,16 @@ class PostController extends Controller
         }
         try{
             $post = Post::find($request->id);
+            if(!$post){
+                return response()->json([
+                    'error' => 'Post not found'
+                ], 404);
+            }
+            if($post->user_id != Auth::user()->id){
+                return response()->json([
+                    'error' => 'You are not authorized to view this post'
+                ], 403);
+            }
             $post->title = $request->title;
             $post->content = $request->content;
 
@@ -67,6 +76,105 @@ class PostController extends Controller
                 ],200);
             }
 
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function editPost2(Request $request, $id){
+        $validated = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            // for method 1
+            // 'id' => 'required|exists:posts,id',
+        ]);
+
+        if($validated->fails()) {
+            return response()->json(['error' => $validated->errors()], 422);
+        }
+        try{
+            $post = Post::find($id);
+            if(!$post){
+                return response()->json([
+                    'error' => 'Post not found'
+                ], 404);
+            }
+            if($post->user_id != Auth::user()->id){
+                return response()->json([
+                    'error' => 'You are not authorized to view this post'
+                ], 403);
+            }
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $updated_post = $post->save();
+            if($updated_post){
+                return response()->json([
+                    'message' => 'Post updated successfully',
+                    'updated_post' => $updated_post,
+                    // 'post' => $post
+                ],200);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function getPosts(){
+        try{
+            $posts = Post::all();
+            if(!$posts){
+                return response()->json([
+                'error' => 'No posts found'
+                ]);
+            }
+            return response()->json([
+                'posts' => $posts
+            ],200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function getPost($id){
+        try{
+            // $post = Post::find($id);
+            $post = Post::where('id', $id)->firstOrFail();
+            if(!$post){
+                return response()->json([
+                    'error' => 'Post not found'
+                ], 404);
+            }
+            return response()->json([
+                'post' => $post
+            ],200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function deletePost($id){
+        try{
+            $post = Post::find($id);
+            if(!$post){
+                return response()->json([
+                    'error' => 'Post not found'
+                ], 404);
+            }
+            if($post->user_id != Auth::user()->id){
+                return response()->json([
+                    'error' => 'You are not authorized to view this post'
+                ], 403);
+            }
+            $post->delete();
+            return response()->json([
+                'message' => 'Post deleted successfully'
+            ],200);
         } catch (\Exception $e) {
             return response()->json([
                 'error' => $e->getMessage()
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/add/post',[PostController::class, 'addNewPost']);
-    // first method for send id of the post
+    // first method for send id of the post is in form request data
     Route::post('/edit/post',[PostController::class, 'editPost']);
+    // seconde method for send id of the post is in url
+    Route::post('/edit2/post/{id}',[PostController::class, 'editPost2']);
+
+    Route::delete('/delete/post/{id}',[PostController::class, 'deletePost']);
 });
+
+Route::get('/posts', [PostController::class, 'getPosts']);
+Route::get('/post/{id}', [PostController::class, 'getPost']);
 
